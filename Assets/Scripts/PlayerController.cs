30416a0ea81ca71d8818ba3afb294b28a41bea66
kyanh <kyanh180320@gmail.com>
2023-05-23 16:27:04 +0700

    Add Jump and animation Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,11 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     Rigidbody2D rb;
     public float speed;
+    public float jumpForce;
+    public Transform groundCheckpoint;
+    public LayerMask whatIsGround;
+    bool isGrounded;
+    bool canJumpDouble;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -15,6 +20,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(speed*Input.GetAxis("Horizontal"),rb.velocity.y);
+        rb.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), rb.velocity.y);
+        isGrounded = Physics2D.OverlapCircle(groundCheckpoint.position, 0.2f, whatIsGround);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                canJumpDouble = true;
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            }
+            else
+            {
+                if (canJumpDouble)
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                    canJumpDouble = false;
+                }
+            }
+        }
     }
 }
