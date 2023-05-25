15acc378921d91e05d1724802f3893225f0b0ec6
kyanh <kyanh180320@gmail.com>
2023-05-25 11:19:20 +0700

    Add spikes and set up health UI

---
+++ Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static PlayerHealthController instance;
+    public int currentHealth, maxHealth;
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void DealDamge()
+    {
+        currentHealth--;
+        if (currentHealth <= 0 )
+        {
+            gameObject.SetActive( false );
+        }
+    }
+}
